b3798fe52f2b07205d7fa112d8e468b413bf1c69
RobsonTrasel <robson.trasel@gmail.com>
2023-08-06 21:30:16 -0300

    feat(navbar): create a new navbar blade

---
+++ resources/views/layouts/parts/navbar/navbar.blade.php
@@ -0,0 +1,45 @@
+<section class="wrapper">
+    <a href="#" class="logo">Milena Melo</a>
+
+
+    <button type="button" class="opened-menu">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </button>
+
+    <div class="overlay"></div>
+
+    <nav class="navbar">
+        <button class="close-menu">
+            <span class="icon-close"></span>
+        </button>
+
+
+        <ul class="menu">
+            <li><a href="#">Espaço</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a data-toggle="sub-menu" href="#">Procedimentos</a></li>
+            <ul class="sub-menu">
+                <li><a href="#">Procedimento 1</a></li>
+                <li><a href="#">Procedimento 1</a></li>
+                <li><a href="#">Procedimento 1</a></li>
+                <li><a href="#">Procedimento 1</a></li>
+            </ul>
+            <li><a href="#">Contato</a></li>
+        </ul>
+
+        <ul class="social-media">
+            <li><a href="#"><span class="icon-instagram"><i
+                            class="fa-brands fa-instagram"></i></span></a>
+            </li>
+            <li><a href="#"><span class="icon-facebook"><i
+                            class="fa-brands fa-facebook-f"></i></span></a>
+            </li>
+            <li><a href="#"><span class="icon-whatsapp"><i
+                            class="fa-brands fa-whatsapp"></i></span></a>
+            </li>
+        </ul>
+    </nav>
+</section>
